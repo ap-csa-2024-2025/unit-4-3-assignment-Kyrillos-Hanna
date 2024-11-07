ca668392fec5f1edde1c03c40092ac12d9917759
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-07 11:20:27 -0800

    worked on 4.3

--- Main.java
+++ Main.java
@@ -4,6 +4,24 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    all25Nums();
+    allNums17to73();
+  }
+
+  public static void all25Nums() {
+    for (int i = 1; i <= 25; i++) {
+      if (i % 2 == 1) {
+        System.out.println(i);
+      }
+    }
+  }
+
+  public static void allNums17to73() {
+    for (int i = 17; i <= 73; i++) {
+      System.out.print(i + " ");
+      if (i % 10 == 6) {
+        System.out.println(" ");
+      }
+    }
   }
 }

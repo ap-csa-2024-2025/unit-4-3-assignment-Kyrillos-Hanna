fcede32bc918b87c60b9b9d61c67d09efce67a3a
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-19 11:16:18 -0800

    finished 4.3

--- Main.java
+++ Main.java
@@ -6,6 +6,15 @@ public class Main
   {
     all25Nums();
     allNums17to73();
+
+    System.out.println("\nEnter a number: ");
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    input0and50(num);
+
+    System.out.println("\nEnter another number: ");
+    int num2 = sc.nextInt();
+    inputGreater0(num2);
   }
 
   public static void all25Nums() {
@@ -21,6 +30,31 @@ public class Main
       System.out.print(i + " ");
       if (i % 10 == 6) {
         System.out.println(" ");
+      }
+    }
+  }
+
+  public static void input0and50(int num) {
+    if (num < 0 || num > 50){
+      System.out.println("error");
+    } else {
+      for (int i = num; i <= 50; i++) {
+        if (i % 5 == (num % 10 + 4) || i % 5 == (num % 10 - 1)) {
+          System.out.println(i);
+        } else {
+          System.out.print(i + " ");
+        }
+      }
+    }
+  }
+
+  public static void inputGreater0(int num) {
+    if (num < 0) {
+      System.out.println("error");
+    } else {
+      num = num - (num % 3);
+      for (int i = num; i >= 0; i-=3) {
+        System.out.print(i + " ");
       }
     }
   }
